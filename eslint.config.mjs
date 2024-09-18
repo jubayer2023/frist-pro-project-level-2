cc765ac3819031ed40551d7f4af3a71a4daa108a
jubayer2023 <jubayersarker400@gmail.com>
2024-09-18 09:32:28 +0600

    basic-project-setup-added

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["src/**/*.{ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      // "no-console": "warn",
+      "no-undef": "error"
+    }
+  },
+  {
+    ignores: ['node_modules', 'dist'],
+  }
+];
